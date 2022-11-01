781bf61030207fbaff1489c69cba83770f9cbecb
Andriyuniversity <andriyuniversity@gmail.com>
2022-11-01 04:49:30 -0700

    init + homework 1 done

---
+++ homeworks/homework1/main.js
@@ -0,0 +1,50 @@
+let hello = 'hello';
+let domain;
+domain = 'owu';
+let tld = 'com';
+let tld_country = 'ua';
+
+let a = 1;
+let b = 10;
+let c = -999;
+let d = 123;
+const PI = 3.14;
+let e = 2.7;
+let f = 16;
+let g = true;
+let h = false;
+
+
+console.log(hello)
+console.log(domain)
+console.log(tld)
+console.log(tld_country)
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(g)
+console.log(h)
+
+let firstName = 'Petro';
+let middleName = 'Perto';
+let lastName = 'Puerto';
+
+person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person)
+
+a = 100;
+b = '100';
+c = true;
+
+console.log(typeof (a))
+console.log(typeof (b))
+console.log(typeof (c))
+
+firstName = prompt('Enter your first name')
+middleName = prompt('Enter your middle name')
+lastName = prompt('Enter your last name')
+
+console.log('Hello' + ' ' + firstName + ' ' + middleName + ' ' + lastName)
