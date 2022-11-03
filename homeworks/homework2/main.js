681c73d2bb6fa107752f969c9eaa6c2844bb437c
Andriyuniversity <andriyuniversity@gmail.com>
2022-11-03 03:40:04 -0700

    homework2 done

---
+++ homeworks/homework2/main.js
@@ -0,0 +1,121 @@
+console.log("----------Task 1------------")
+let x = false;
+let arr = [true, 2, 'greenfield', ['honey', false, farms = {
+    lviv: 1,
+    lytck: 2,
+    gorp: 4
+}, 22,],
+    house = {
+        door: "yellow",
+        floor: "blue",
+    }, 22, 'asdfa', x, 932.90, 'oksdfa', assdf = ['asdfas', 'asdfasd', 'dffgke']]
+
+console.log(arr [0]);
+console.log(arr [1]);
+console.log(arr [2]);
+console.log(arr [3]);
+console.log(arr [4]);
+console.log(arr [5]);
+console.log(arr [6]);
+console.log(arr [7]);
+console.log(arr [8]);
+console.log(arr [9]);
+console.log(arr [10]);
+
+console.log("Звернення до об'єкту farm, ключа lytck:", arr[3][2].lytck)
+
+console.log("----------Task 2------------")
+
+let book1 = {
+    title: "adfgjkndew",
+    pageCount: 567,
+    genre: "fiction",
+}
+
+let book2 = {
+    title: "qwegdsfdew",
+    pageCount: 165,
+    genre: "novel",
+}
+
+let book3 = {
+    title: "p[oik,jb",
+    pageCount: 88,
+    genre: "narrative",
+}
+
+console.log(book1, book2, book3)
+
+console.log("----------Task 3------------")
+
+book1.authors = ["Hendelf", 55,];
+
+book2["authors"] = ["Paul", 43,];
+
+book3["authors"] = ["Lorem", 63]
+
+console.log(book1, book2, book3)
+
+console.log("----------Task 4------------")
+
+let users = [user1 = {
+    name: "someName",
+    username: "greq",
+    password: 'eg34tsg',
+},
+    user2 = {
+        name: "someName",
+        username: "gsagq",
+        password: 'qwertga',
+    },
+    user3 = {
+        name: "someName",
+        username: "greq",
+        password: 'eg34tsg',
+    },
+    user4 = {
+        name: "someName",
+        username: "greq",
+        password: 'mvkls;dflk',
+    },
+    user5 = {
+        name: "someName",
+        username: "greq",
+        password: 'poqm;lkm',
+    },
+    user6 = {
+        name: "someName",
+        username: "greq",
+        password: 'oiiknjfgs',
+    },
+    user7 = {
+        name: "someName",
+        username: "greq",
+        password: 'qmnbsrt',
+    },
+    user8 = {
+        name: "someName",
+        username: "greq",
+        password: 'i768ibvs',
+    },
+    user9 = {
+        name: "someName",
+        username: "greq",
+        password: 'q4hjntw3',
+    },
+    user10 = {
+        name: "someName",
+        username: "greq",
+        password: '1234579',
+    },]
+
+console.log(user1.password)
+console.log(user2.password)
+console.log(user3.password)
+console.log(user4.password)
+console.log(user5.password)
+console.log(user6.password)
+console.log(user7.password)
+console.log(user8.password)
+console.log(user9.password)
+console.log(user10.password)
